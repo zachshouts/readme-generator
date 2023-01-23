59c3a05ff8717519ee5a99790eb9876f0c2e512c
zachshouts <117788174+zachshouts@users.noreply.github.com>
2023-01-23 17:50:20 -0600

    Connected index.js and generateMarkdown.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generate = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -9,52 +9,55 @@ const questions = [
         message: 'Enter your project title.',
         name: 'title'
     },
-    {
-        type: 'input',
-        message: 'Enter your project description.',
-        name: 'description'
-    },
-    {
-        type: 'input',
-        message: 'Enter installation instructions.',
-        name: 'installation'
-    },
-    {
-        type: 'input',
-        message: 'Enter usage instructions',
-        name: 'usage'
-    },
-    {
-        type: 'list',
-        message: 'Choose a license',
-        choices: ['None', 'Apache', 'MIT', 'Boost'],
-        name: 'license'
-    },
-    {
-        type: 'input',
-        message: 'Enter contributing information.',
-        name: 'contributing'
-    },
-    {
-        type: 'input',
-        message: 'Enter testing information.',
-        name: 'testing'
-    },
-    {
-        type: 'input',
-        message: 'Enter questions info.',
-        name: 'questions'
-    }
+    // {
+    //     type: 'input',
+    //     message: 'Enter your project description.',
+    //     name: 'description'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Enter installation instructions.',
+    //     name: 'installation'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Enter usage instructions',
+    //     name: 'usage'
+    // },
+    // {
+    //     type: 'list',
+    //     message: 'Choose a license',
+    //     choices: ['None', 'Apache', 'MIT', 'Boost'],
+    //     name: 'license'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Enter contributing information.',
+    //     name: 'contributing'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Enter testing information.',
+    //     name: 'testing'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Enter questions info.',
+    //     name: 'questions'
+    // }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => err ? console.log(err) : console.log('Success'));
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
         .then((response) => {
-            console.log(response);
+            const markdown = generateMarkdown(response);
+            writeToFile('generated-readme.md', markdown);
         });
 }
 
